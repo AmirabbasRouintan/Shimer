b9764ea9929ebfd2e12af0ae2c50edd119917d10
AmirabbasRouintan <amirabbas.helma8693@gmail.com>
2026-07-05 20:03:44 +0330

    chore: set Google OAuth credentials

--- constants/auth.ts
+++ constants/auth.ts
@@ -1,4 +1,4 @@
 export const AUTH_CONFIG = {
   BACKEND_URL: "https://shimer-api.vercel.app",
-  GOOGLE_CLIENT_ID: "your_google_client_id_here",
+  GOOGLE_CLIENT_ID: "487910244656-3m5lmelvqg212l352gr2g2f7em0r9ajb.apps.googleusercontent.com",
 };
